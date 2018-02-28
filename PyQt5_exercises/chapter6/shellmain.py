b25034cbbaa74b2a02721a0ec1a0c400731a8f90
Gaynetdin <m.gaynutdinov@mail.ru>
2018-03-01 03:34:28 +0400

    6.4 exercise completed

--- PyQt5_exercises/chapter6/shellmain.py
+++ PyQt5_exercises/chapter6/shellmain.py
@@ -5,6 +5,7 @@ import xml.dom.minidom
 from shell import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class MyWin(QtWidgets.QMainWindow):
     
     lbs = []
@@ -15,15 +16,28 @@ class MyWin(QtWidgets.QMainWindow):
     labels = [] 
     variants = []
     correct = []
-    
+    clickedAnswer = 0
+    checkList = []
+
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        #self.progressBar = QtWidgets.QProgressBar(self.ui.statusbar)
+        #self.ui.statusbar.addWidget(self.progressBar)
+
+        self.progressBar = QtWidgets.QProgressBar(self.ui.centralwidget)
+        #self.progressBar.setMaximumSize(QtCore.QSize(75, 75))
+        self.progressBar.setObjectName("ProgressBar")
+        self.progressBar.setGeometry(50, 50, 20, 25)
+        self.ui.gridLayout.addWidget(self.progressBar,1,0,1,1)
+        
+        
         #substitute statusbar with labelStasus cause statusbar doesn't support rich text
         self.labelStatus = QtWidgets.QLabel(self.ui.statusbar)
-        self.ui.statusbar.addWidget(self.labelStatus) 
+        self.ui.statusbar.addWidget(self.labelStatus)
+
         # xml handling (read & mix)
         self.mixXml()
         # read to DOM
@@ -88,22 +102,38 @@ class MyWin(QtWidgets.QMainWindow):
             self.lbs[line].setText('<b>%s</b>' % self.labels[line])
             self.verticalLayout.addWidget(self.lbs[line])
             self.bgrs.append(QtWidgets.QButtonGroup(self.ui.centralwidget))
+
+            self.bgrs[line].buttonClicked.connect(self.progressUpdate) 
+
             for v in range(0, len(self.variants[line])):
                 self.rbs[line][v] = QtWidgets.QRadioButton(self.ui.scrollAreaWidgetContents)
                 self.bgrs[line].addButton(self.rbs[line][v])
                 self.rbs[line][v].setText(self.variants[line][v])
                 self.verticalLayout.addWidget(self.rbs[line][v])
     
+    #group.buttonClicked['QAbstractButton *'].connect(self.groupButtonClicked)
+    def progressUpdate(self):
+        counterPrgr = 0
+        for bg in self.bgrs:
+            for rb in bg.buttons():
+                if rb.isChecked():
+                    counterPrgr += 1
+                    self.progressBar.setValue( int(counterPrgr/len(self.labels)*100) )
+                    self.progressBar.update() 
+
+
     def check(self):
         counter = 0
         for group in range(0, len(self.bgrs)):
             for rb in self.bgrs[group].buttons():
                 if rb.isChecked():
+                    #clickedAnswer +=1
                     if rb.text() == self.correct[group]:
                         counter += 1
         # And this is the result! Rounded to 2 decimal points
         result = round(float(counter/len(self.bgrs)*100),2)
-
+        #if (clickedAnswer!=0): 
+        #    self.progressBar.setValue((clickedAnswer*100)/self.numQ)
         if (result<=50): #red
             message = "Your result is " + "<b style=color:'red';>%.2f</b>" % float(counter/len(self.bgrs)*100) + "%" 
         elif (result<=75): #yellow
@@ -112,6 +142,8 @@ class MyWin(QtWidgets.QMainWindow):
             message = "Your result is " + "<b style=color:'green';>%.2f</b>" % float(counter/len(self.bgrs)*100) + "%" 
 
         self.labelStatus.setText(message)
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
