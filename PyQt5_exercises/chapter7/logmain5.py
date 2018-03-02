c7b91b243d29f995dc48bc5fa46b929f8cab981f
Gaynetdin <m.gaynutdinov@mail.ru>
2018-03-03 01:28:39 +0400

    completed 6th unit

---
+++ PyQt5_exercises/chapter7/logmain5.py
@@ -0,0 +1,172 @@
+import sys
+import random
+import os
+import io
+import time
+import xml.dom.minidom
+from shell2 import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MyWin(QtWidgets.QMainWindow):
+    
+    lbs = []
+    rbs = [[''] * 10] * 15 # emply list 15x10
+    bgrs = []
+    labels = [] 
+    variants = []
+    correct = []
+    logStr = ''
+    triggeredNum = 0
+    
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # creating timer
+        self.timer = QtCore.QTimer(self)
+        # xml handling (read & mix)
+        self.mixXml()
+        # read to DOM
+        self.readToDom()
+        # assigning layout to the scrollarea
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.ui.scrollAreaWidgetContents)
+        self.verticalLayout.setObjectName("verticalLayout")
+        # adding widgets to the scrollarea
+        self.addWidgetsToInterface()
+        self.timer.timeout.connect(lambda: self.updater(self.timeSeconds))
+        # starting timer
+        self.timer.start(1000)
+                
+        self.ui.pushButton.clicked.connect(self.check)
+                
+    def mixXml(self):
+        # read xml and mix the lines
+        self.linesMixed = []
+        self.r = open("db1.xml", 'r', encoding='utf-8')
+        self.fileRead = self.r.readlines()
+        for line in range(2, len(self.fileRead)-1):
+            self.linesMixed.append(self.fileRead[line])
+        random.shuffle(self.linesMixed)
+        self.r.close()
+        
+        # write temporary xml with new mixed lines
+        self.w = open("temp.xml", 'w', encoding='utf-8')
+        self.w.write('''<?xml version="1.0" encoding="utf-8"?>\n<content>\n''')
+        for line in self.linesMixed:
+            self.w.write('%s' % line)
+        self.w.write('</content>')
+        self.w.close()
+        
+    def readToDom(self):
+        # read to DOM
+        self.dom = xml.dom.minidom.parse('temp.xml')
+        self.collection = self.dom.documentElement
+        # reading timeout to a variable
+        self.timeSeconds = int(self.collection.getElementsByTagName("time")[0].childNodes[0].data)
+        self.timeConstant = self.timeSeconds
+        self.linesArr = self.collection.getElementsByTagName("q")
+        for line in range(0, len(self.linesArr)):
+            # label's text
+            self.labels.append(self.linesArr[line].childNodes[0].data)
+            # variants' text
+            self.variants.append(self.linesArr[line].getAttribute('ans').split('**?**'))
+            # correct answer
+            self.correct.append(self.linesArr[line].getAttribute('cor'))
+        # Mix variants
+        for variant in self.variants:
+            random.shuffle(variant)
+        # Deleting temporary file
+        os.remove('temp.xml')
+    
+    def addWidgetsToInterface(self):
+        # adding widgets to the scrollarea
+        for line in range (0, len(self.labels)):
+            self.lbs.append(QtWidgets.QLabel(self.ui.scrollAreaWidgetContents))
+            self.lbs[line].setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+            self.lbs[line].setText('<b>%s</b>' % self.labels[line])
+            self.verticalLayout.addWidget(self.lbs[line])
+            self.bgrs.append(QtWidgets.QButtonGroup(self.ui.centralwidget))
+            self.bgrs[line].buttonClicked.connect(self.increaseNum)
+            for v in range(0, len(self.variants[line])):
+                self.rbs[line][v] = QtWidgets.QRadioButton(self.ui.scrollAreaWidgetContents)
+                self.bgrs[line].addButton(self.rbs[line][v])
+                self.rbs[line][v].setText(self.variants[line][v])
+                self.verticalLayout.addWidget(self.rbs[line][v])
+    
+    def increaseNum(self):
+        self.triggeredNum += 1
+
+    def check(self):
+        counter = 0
+        for group in range(0, len(self.bgrs)):
+            correctThis = '--'
+            # adding questions
+            self.logStr += self.labels[group] + '\n'
+            for rb in self.bgrs[group].buttons():
+                # adding variants
+                self.logStr += '\t' + rb.text() + '\n'
+                if rb.isChecked():
+                    if rb.text() == self.correct[group]:
+                        correctThis = rb.text()
+                        counter += 1
+            self.logStr += 'Правильный ответ: %s' % self.correct[group] + '\n'
+            self.logStr += 'Ответ пользователя: %s' % correctThis + '\n'
+            self.logStr += '\n'
+        # And this is the result! Rounded to 2 decimal points
+        self.result = float(counter/len(self.bgrs)*100)
+        message = "Your result is " + "%.2f" % self.result + "%"
+        self.ui.statusbar.setStyleSheet('color: navy; font-weight: bold;')
+        self.ui.statusbar.showMessage(message)
+        # creating log file
+        self.log()
+                
+    def updater(self, val):
+        val = self.timeSeconds
+        if val == 0:
+            self.timer.stop()
+            self.check()
+            self.ui.scrollArea.setEnabled(False)
+            self.ui.pushButton.setEnabled(False)
+        self.ui.label.setText(self.intToTime(val))
+        self.timeSeconds -= 1
+        
+    def intToTime(self, num):
+        h = 0
+        m = 0
+        if num >= 3600:
+            h = num // 3600
+            num = num % 3600
+        if num >= 60:
+            m = num // 60
+            num = num % 60
+        s = num
+        str1 = "%d." % h
+        if m < 10:
+            str1 += "0%d:" % m
+        else:
+            str1 += "%d:" % m
+        if s < 10:
+            str1 += "0%d" % s
+        else:
+            str1 += "%d" % s
+        return str1 # returns time as a string
+
+    def log(self):
+        file = open("log.txt", 'w', encoding='utf-8')
+        file.write("Дата и время записи: ")
+        file.write(time.strftime("%Y-%m-%d %H:%M:%S"))
+        file.write('\n\n')
+        file.write(self.logStr)
+        file.write('\n')
+        file.write("Результат: %.2f" % self.result + " %\n")
+        file.write('Выполнено за %d секунд\n' % (self.timeConstant - self.timeSeconds))
+        file.write('Количество действий: %d' % self.triggeredNum)
+        file.close()
+        self.triggeredNum = 0
+        self.logStr = ''
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ PyQt5_exercises/chapter7/shell2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'shell2.ui'
+#
+# Created: Thu May  8 23:02:31 2014
+#      by: PyQt5 UI code generator 5.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 580, 391))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 2, 0, 1, 2)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setMaximumSize(QtCore.QSize(75, 16777215))
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 3, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setText("")
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Check"))
+
