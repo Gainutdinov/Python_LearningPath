eafa2a6d7f4af2c25024077965dcdb0038ec15d3
Gainutdinov <m.gaynutdinov@mail.ru>
2018-12-15 22:57:30 +0300

    added few comments.

--- PyQt5_exercises/PyQt5_Advanced/DefaultWidgets/week8/eventFilterExample.py
+++ PyQt5_exercises/PyQt5_Advanced/DefaultWidgets/week8/eventFilterExample.py
@@ -15,7 +15,7 @@ class simpleWidget(QWidget):
     def eventFilter(self, obj, event): # obligatory method to catch events  
         if event.type() == QEvent.Enter:
             self.label.setText('IN')
-            return True # used to stop catching the event by another function
+            return True # used to stop catching the event by another function, additionally, cause event is raised earlier before information send to our object (in our case to simpletWidget class) we need to explain to engine that we handled this event and there is no need to send event to another object (we return True), then we do not handle event it should be received by original object and by returning False we literally say that we will not handle the event our object (simpleWidget instance)
         elif event.type() == QEvent.Leave:
             self.label.setText('OUT')
             return True
